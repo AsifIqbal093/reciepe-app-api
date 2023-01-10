0b43aba59243303507f789a408885ec4ac93af0f
AsifIqbal093 <im.asif093@gmail.com>
2023-01-10 14:40:44 +0500

    User Model Integration and Admin Customization completed.

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,45 @@
-from django.contrib import admin # noqa
+"""
+Django Admin Customization.
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (_('Credentials'), {'fields':('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields':(
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Last Login'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
+
+admin.site.register(models.User, UserAdmin)
